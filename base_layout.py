fac932edab5a52a15b26be123549644a10848da7
matfelhau <mathiashaugen02@gmail.com>
2023-10-13 11:25:20 +0200

    Starta på prosjektet med base layout og json

---
+++ base_layout.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+
+
+def home(self):
+    self.title("Tourism marketplace")
+    self.geometry("500x500")
+
+class BaseLayout(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        home(self)
+
+if __name__ == "__main__":
+    base = BaseLayout()
+    base.mainloop()
