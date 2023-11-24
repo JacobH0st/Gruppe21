196b6d39c43c6c645a34fc51715d1b6cbdabf08a
Mathias Felde Haugen <111960714+matfelhau@users.noreply.github.com>
2023-11-24 04:22:30 +0100

    La til flere tester og fikset en bug

--- functions/create_data_entry_form.py
+++ functions/create_data_entry_form.py
@@ -7,37 +7,37 @@ class CreateDataEntryFormBase:
     def __init__(self, app, frame):
         self.app = app
         self.frame = frame
-        self.create_data_entry_form()
         
-    def create_data_entry_form(self):   
+        self.company_entry = Entry(self.frame)
+        self.datetime_entry = Entry(self.frame)
+        self.duration_entry = Entry(self.frame)
+        self.phone_entry = Entry(self.frame)
+        self.address_entry = Entry(self.frame)
+        self.price_entry = Entry(self.frame)
+        self.remaining_seats_entry = Entry(self.frame)
+        self.total_seats_entry = Entry(self.frame)
+        self.age_limit_entry = Entry(self.frame)
+        self.outdoor_indoor_entry = Entry(self.frame)
+        self.rating_entry = Entry(self.frame)
+        self.type_entry = Entry(self.frame)
+        self.description_entry = Entry(self.frame)
+                
         company_entry = Entry(self.frame)
-        datetime_entry = Entry(self.frame)
-        duration_entry = Entry(self.frame)
-        phone_entry = Entry(self.frame)
-        address_entry = Entry(self.frame)
-        price_entry = Entry(self.frame)
-        remaining_seats_entry = Entry(self.frame)
-        total_seats_entry = Entry(self.frame)
-        age_limit_entry = Entry(self.frame)
-        outdoor_indoor_entry = Entry(self.frame)
-        rating_entry = Entry(self.frame)
-        type_entry = Entry(self.frame)
-        description_entry = Entry(self.frame)
-        
+
         fields = [
-            ("Company", 0, company_entry),
-            ("Date and Time", 1, datetime_entry),
-            ("Duration", 2, duration_entry),
-            ("Phone", 3, phone_entry),
-            ("Address", 4, address_entry),
-            ("Price", 5, price_entry),
-            ("Remaining Seats", 6, remaining_seats_entry),
-            ("Total Seats", 7, total_seats_entry),
-            ("Age Limit", 8, age_limit_entry),
-            ("Outdoor/Indoor", 9, outdoor_indoor_entry),
-            ("Rating", 10, rating_entry),
-            ("Type", 11, type_entry),
-            ("Description", 12, description_entry),
+            ("Company", 0, self.company_entry),
+            ("Date and Time", 1, self.datetime_entry),
+            ("Duration", 2, self.duration_entry),
+            ("Phone", 3, self.phone_entry),
+            ("Address", 4, self.address_entry),
+            ("Price", 5, self.price_entry),
+            ("Remaining Seats", 6, self.remaining_seats_entry),
+            ("Total Seats", 7, self.total_seats_entry),
+            ("Age Limit", 8, self.age_limit_entry),
+            ("Outdoor/Indoor", 9, self.outdoor_indoor_entry),
+            ("Rating", 10, self.rating_entry),
+            ("Type", 11, self.type_entry),
+            ("Description", 12, self.description_entry),
         ]
         
         for field, row, entry_widget in fields:
@@ -50,22 +50,24 @@ class CreateDataEntryFormBase:
             elif field == "Rating":
                 entry_widget.insert(0, "0.0")
                 
-        add_button = Button(self.frame, text="Add Data", command=lambda: add_data_to_json(
+        self.add_button = Button(self.frame, text="Add Data", command=lambda: add_data_to_json(
             company_entry, 
-            datetime_entry, 
-            duration_entry, 
-            phone_entry, 
-            address_entry, 
-            price_entry,
-            remaining_seats_entry, 
-            total_seats_entry, 
-            age_limit_entry, 
-            outdoor_indoor_entry,
-            rating_entry, 
-            type_entry, 
-            description_entry
+            self.datetime_entry, 
+            self.duration_entry, 
+            self.phone_entry, 
+            self.address_entry, 
+            self.price_entry,
+            self.remaining_seats_entry, 
+            self.total_seats_entry, 
+            self.age_limit_entry, 
+            self.outdoor_indoor_entry,
+            self.rating_entry, 
+            self.type_entry, 
+            self.description_entry
             ))
-        add_button.grid(row=13, column=0, columnspan=2, padx=10, pady=10)
+        self.add_button.grid(row=13, column=0, columnspan=2, padx=10, pady=10)
 
-        back_button = Button(self.frame, text="Back", command=lambda: self.app.switch_to_main_frame())
-        back_button.grid(row=15, column=0, columnspan=2, padx=10, pady=10)
+        self.back_button = Button(self.frame, text="Back", command=lambda: self.app.switch_to_main_frame())
+        self.back_button.grid(row=15, column=0, columnspan=2, padx=10, pady=10)
+
+        
--- tests/test_create_data_entry_form.py
+++ tests/test_create_data_entry_form.py
@@ -1,1 +1,40 @@
+from tkinter import Button, Entry, Frame, Tk
+import pytest
 
+from functions.create_data_entry_form import CreateDataEntryFormBase
+
+@pytest.fixture
+def create_data_entry_form():
+    root = Tk()
+    frame = Frame(root)
+    data_entry_form = CreateDataEntryFormBase(root, frame)
+    return data_entry_form
+
+#WIDGETS:
+
+#Testing the widgets to check if they are created.
+def test_is_create_data_entry_form_widgets_created(create_data_entry_form):
+    assert isinstance(create_data_entry_form.company_entry, Entry)
+    assert isinstance(create_data_entry_form.datetime_entry, Entry)
+    assert isinstance(create_data_entry_form.duration_entry, Entry)
+    assert isinstance(create_data_entry_form.phone_entry, Entry)
+    assert isinstance(create_data_entry_form.address_entry, Entry)
+    assert isinstance(create_data_entry_form.price_entry, Entry)
+    assert isinstance(create_data_entry_form.remaining_seats_entry, Entry)
+    assert isinstance(create_data_entry_form.total_seats_entry, Entry)
+    assert isinstance(create_data_entry_form.age_limit_entry, Entry)
+    assert isinstance(create_data_entry_form.outdoor_indoor_entry, Entry)
+    assert isinstance(create_data_entry_form.rating_entry, Entry)
+    assert isinstance(create_data_entry_form.type_entry, Entry)
+    assert isinstance(create_data_entry_form.description_entry, Entry)
+    
+#Testing to see if the buttons are created
+def test_is_create_data_entry_form_buttons_created(create_data_entry_form):
+    assert isinstance(create_data_entry_form.add_button, Button)
+    assert isinstance(create_data_entry_form.back_button, Button)
+
+#Testing default values
+def test_default_values(create_data_entry_form):
+    assert create_data_entry_form.remaining_seats_entry.get() == "0"
+    assert create_data_entry_form.total_seats_entry.get() == "0"
+    assert create_data_entry_form.rating_entry.get() == "0.0"
